b174697fcfe068a1473dba5f1e3661d1c432e7f1
Emmalynn Jenkins <jtenjt6@gmail.com>
2019-10-01 16:59:57 -0600

    Messages/elseifstatements
    Made text show up on console with some easter eggs, learning from udemy tutorial about else if statements again, refreshing everything.

--- Terminal_Hacker_Game/Assets/WM2000/Hacker.cs
+++ Terminal_Hacker_Game/Assets/WM2000/Hacker.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Hacker : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         ShowMainMenu();
     }
-	void ShowMainMenu  ()  {
+	void ShowMainMenu()
+    {
         Terminal.ClearScreen();
-        string greeting = "Welcome User";
-        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Welcome User");
         Terminal.WriteLine("What Would You Like to Hack Into?");
         Terminal.WriteLine("Press 1, for the Police Station");
         Terminal.WriteLine("Press 2, for the treasury");
@@ -19,10 +20,24 @@ public class Hacker : MonoBehaviour {
     }
     //Learned about how variables work today. Variables are like boxes, containing things like amount of lives, speed amounts, many things
     //a String is a string of characters. like "hello" string of letters
+    void OnUserInput(string input)
+    {
+        //Terminal.WriteLine("User has typed..." + input);
+        if (input == "Menu")
+        {
+            ShowMainMenu();
+        }
+        else if (input == "UwU")
+        {
+            Terminal.WriteLine("OwO Whats this? wrong wevel sewection! pwease imput a vawid wevel pweeeeeze... UwU");
+        }
+        else
+        {
+            Terminal.WriteLine("Please select a valid level entry, type 'Menu' to return to main menu.");
+        }
+    }
+    //Functions vs messages, using input as what the "player" types, making it print out on the console and the terminal.
+    //messages get called from the outside, like an island, the message comes in. the "input" then is called to wherever. (console, terminal)
+    //boolean, or bool. true or false.
 
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
